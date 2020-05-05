981b6c73d938a2a0946dc6d7dca73d9a03cea3e0
kharioki <antolisah@gmail.com>
2020-05-05 17:25:13 +0300

    useState hooks and uuid

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v1 as uuidv1 } from 'uuid';
 
 function SongList() {
+  const [songs, setSongs] = useState([
+    { title: 'almost home', id: 1 },
+    { title: 'memory gospel', id: 2 },
+    { title: 'this wild darkness', id: 3 }
+  ]);
+
+  const addSong = () => {
+    setSongs([...songs, { title: 'Joro', id: uuidv1() }]);
+  };
+
   return (
     <div className="song-list">
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map(song => (
+          <li key={song.id}>{song.title}</li>
+        ))}
       </ul>
+      <button onClick={addSong}>Add a Song</button>
     </div>
   );
 }
