18fbb0305ca4268a58583821556bdfca71d335f7
kharioki <antolisah@gmail.com>
2020-05-05 17:08:05 +0300

    project init

---
+++ src/components/SongList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function SongList() {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>this wild darkness</li>
+        <li>memory gospel</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SongList;
